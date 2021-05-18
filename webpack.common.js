2ab87c55d3b0f7617fb63424067b2e586b77ba00
Michał Długosz <mdugosz@gmail.com>
2021-05-19 00:13:57 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+      template: 'src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
