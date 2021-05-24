d4f90e20f7b5703592076d8e3bb9b4cce4237a13
Michał Długosz <mdugosz@gmail.com>
2021-05-24 18:18:31 +0200

    Add function for starting app and event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import css from './style.css';
+import {
+  getWeatherData,
+  getLocationDetails,
+  getUserPosition,
+} from './modules/data';
+
+async function appStart() {
+  try {
+    const userPosition = await getUserPosition();
+    let coords = {
+      lat: userPosition.coords.latitude,
+      lon: userPosition.coords.longitude,
+    };
+
+    const getCity = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=027f71f3deef4424ea3b9f0d6b11dc68`
+    );
+    const getCityObject = await getCity.json();
+    const cityAndCountry = {
+      name: getCityObject.name,
+      country: getCityObject.sys.country,
+    };
+
+    Object.assign(coords, cityAndCountry);
+    getWeatherData(coords);
+  } catch {
+    let defaultCoords = {
+      lat: 54.548,
+      lon: 18.552,
+      name: 'Gdynia',
+      country: 'PL',
+    };
+
+    getWeatherData(defaultCoords);
+  }
+}
+
+appStart();
+
+const searchBar = document.querySelector('.search-bar');
+searchBar.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const whatewa = getLocationDetails(searchBar.value);
+    getWeatherData(whatewa);
+  }
+});
+
+const buttons = document.querySelectorAll('button');
+const details = document.querySelectorAll('.forecast-details');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (button.classList.contains('active')) {
+      return;
+    } else {
+      for (let i = 0; i < 2; i++) {
+        buttons[i].classList.toggle('active');
+        details[i].classList.toggle('show');
+      }
+    }
+  });
+});
